d9d95e0756824b0e58fd1115b6acaadbb1d12b0a
Rui Miguel Abrantes <rmigueab@emeal.nttdata.com>
2023-11-30 16:32:43 +0000

    Remove all comments

--- BoardUserInterface.API/UploadFiles/ExcelMetadataService.cs
+++ BoardUserInterface.API/UploadFiles/ExcelMetadataService.cs
@@ -12,47 +12,6 @@ public interface IExcelMetadataService
 
 public class ExcelMetadataService : IExcelMetadataService
 {
-    /*
-    public string GetVersionNumberFromExcel(Stream excelFileStream)
-    {
-
-
-        using (var document = SpreadsheetDocument.Open(excelFileStream, false))
-        {
-            var extendedFilePropertiesPart = document.ExtendedFilePropertiesPart;
-
-            if (extendedFilePropertiesPart != null)
-            {
-                // Load the properties
-                var properties = extendedFilePropertiesPart.Properties.Elements<Properties>().FirstOrDefault();
-
-                if (properties != null)
-                {
-                    // Access the properties, including the version
-                    string version = properties.Application.ToString();
-                    string title = properties.TitlesOfParts.ToString();
-                }
-            }
-
-            var customProps = document.CustomFilePropertiesPart;
-            if (customProps != null)
-            {
-                var props = customProps.Properties;
-                if (props != null)
-                {
-                    // Assuming 'Version' is the property name for the version number in metadata
-                    var versionProp = props.FirstOrDefault(p => p.XName == "Version");
-                    if (versionProp != null)
-                    {
-                        return versionProp.InnerText;
-                    }
-                }
-            }
-        }
-        return null; // Return null if no version number is found
-    }
-    */
-
     public string GetVersionNumberFromExcel(Stream excelFileStream)
     {
         using (var document = SpreadsheetDocument.Open(excelFileStream, false))
