a0ad23215f70e20fc025408f356ed5e0444e071f
Rui Miguel Abrantes <rmigueab@emeal.nttdata.com>
2023-12-27 10:02:10 +0000

    Added expcetions to new logs

--- BoardUserInterface.API/Middlewares/ExceptionMiddleware.cs
+++ BoardUserInterface.API/Middlewares/ExceptionMiddleware.cs
@@ -1,4 +1,6 @@
-﻿using BoardUserInterface.Service.Logging;
+﻿using BoardUserInterface.Service.DataAccess;
+using BoardUserInterface.Service.Logging;
+using BoardUserInterfaces.DataAccess.Models;
 using DocumentFormat.OpenXml.Office2016.Excel;
 
 public class ExceptionMiddleware
@@ -6,12 +8,15 @@ public class ExceptionMiddleware
 
     private readonly RequestDelegate _next;
     private readonly ILogService _logService;
+    private readonly ILogsRepoService _logsRepoService;
 
-    public ExceptionMiddleware(RequestDelegate next,ILogService logService )
+    public ExceptionMiddleware(RequestDelegate next,ILogService logService, ILogsRepoService logsRepoService)
     {
         _next = next;
         _logService = logService;
-    }
+
+        _logsRepoService = logsRepoService;
+}
 
     public async Task InvokeAsync(HttpContext httpContext)
     {
@@ -34,6 +39,14 @@ public class ExceptionMiddleware
 
         // You can log the exception here if needed (e.g., to a file or database)
         _logService.LogMessage("Backend", "Not Successful", exception.Message, "Error");
+        var newLog = new Logs
+        {
+            Source = "Backend",
+            Context = "Exception",
+            Message = "Download successful",
+            Type = "Error"
+        };
+        _ = _logsRepoService.CreateLogAsync(newLog);
         // _logger.LogError(exception, exception.Message);
 
         return context.Response.WriteAsync(errorResponse.ToString());
